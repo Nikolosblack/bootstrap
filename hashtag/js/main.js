3c8af1292838beb81d95c98b6ec2e7c804b6c3de
Nikolos Black <Nikolosblack@gmail.com>
2022-09-06 18:15:13 +0300

    add new project with header,banner,carousel sections and css styles, add some js to navbar and carousel

---
+++ hashtag/js/main.js
@@ -0,0 +1,42 @@
+const nav = document.querySelector(".navbar");
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// Nav hide
+const navBar = document.querySelectorAll(".nav-link");
+const navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+    item:4,
+    loop:true,
+    nav: false,
+    autoplay:true,
+    autoplaytimeout:2000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0: {
+            items: 1
+        },
+        400: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
